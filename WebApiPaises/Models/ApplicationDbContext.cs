c6bcc897bab039e82559c6895e66bb837a62b51c
Felipe Gavilan <elmejor_887@hotmail.com>
2018-01-12 12:54:50 -0400

    Add project files.

---
+++ WebApiPaises/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiPaises.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Pais> Paises { get; set; }
+        public DbSet<Provincia> Provincias { get; set; }
+    }
+}
---
+++ WebApiPaises/Models/Provincia.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiPaises.Models
+{
+    public class Provincia
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        [ForeignKey("Pais")]
+        public int PaisId { get; set; }
+        [JsonIgnore]
+        public Pais Pais { get; set; }
+    }
+}
